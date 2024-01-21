307fe2bd50855bcaba71dcd35ac5dd1d38611a68
Joel Mondalgo <jmondalgoc@gmail.com>
2024-01-21 12:56:02 -0500

    [let] keyword

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //Variable declarations
+
+//var
+var variable = "var declara una variable con ámbito de función o ámbito global";
+
+//let 
+let variableLet = "let declara una variable local con ámbito de bloque"
