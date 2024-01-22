9d1f264f0888bf247c0adb595bdeac32af436e18
Joel Mondalgo <jmondalgoc@gmail.com>
2024-01-22 13:39:18 -0500

    [const] keyword

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ var variable = "var declara una variable con ámbito de función o ámbito globa
 
 //let 
 let variableLet = "let declara una variable local con ámbito de bloque"
+
+//const
+const constante = "no se puede cambiar mediante reasignación ni volver a declarar, tiene alcance de bloque"
