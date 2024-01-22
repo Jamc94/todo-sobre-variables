bd146ca0f7150bfbf2353d3427d6421268cbf1bd
Joel Mondalgo <jmondalgoc@gmail.com>
2024-01-22 16:49:32 -0500

    variable naming rules

--- script.js
+++ script.js
@@ -4,7 +4,14 @@
 var variable = "var declara una variable con ámbito de función o ámbito global";
 
 //let 
-let variableLet = "let declara una variable local con ámbito de bloque"
+let variableLet = "let declara una variable local con ámbito de bloque";
 
 //const
-const constante = "no se puede cambiar mediante reasignación ni volver a declarar, tiene alcance de bloque"
+const constante = "no se puede cambiar mediante reasignación ni volver a declarar, tiene alcance de bloque";
+
+
+//Variable Naming Rules
+
+let camelCase = ["comienza con letra minúscula","la primera letra de cada palabra subsiguiente en mayúscula"];
+
+const SCREAMING_SNAKE_CASE = ["todas las letras en mayúsculas","separadas por guiones bajos"];
